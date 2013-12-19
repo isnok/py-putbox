52990353a6826f0c4d711f906b59997bf2ce2909
Konstantin Martini <k@tuxcode.org>
2013-12-19 12:11:53 +0100

    serving default resource

--- src/putbox.py
+++ src/putbox.py
@@ -8,12 +8,14 @@ if args.debug:
 
 from twisted.internet import reactor
 from twisted.web.server import Site
+from twisted.web.resource import Resource
 
+mainsite = Site(Resource())
+reactor.listenTCP(args.port, mainsite, interface=args.interface)
 
-def test(*args):
-    log.msg("test: %s" % (args,))
-    reactor.stop()
+#def test(*args):
+    #log.msg("test: %s" % (args,))
+    #reactor.stop()
+#reactor.callLater(3, test)
 
-
-reactor.callLater(3, test)
 reactor.run()
